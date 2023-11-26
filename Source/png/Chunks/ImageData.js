a9af9e5d68c627a24b8199614322b600346b167c
Ellie Fagerberg <hi@ellie.moe>
2023-11-26 10:48:56 +0100

    Remove logs

--- Source/png/Chunks/ImageData.js
+++ Source/png/Chunks/ImageData.js
@@ -1,77 +1,55 @@
+import { inflate } from "https://deno.land/std@0.162.0/node/zlib.ts";
+import { zlib } from "https://deno.land/std@0.162.0/node/internal_binding/constants.ts";
 
-import { inflate } from 'https://deno.land/std@0.162.0/node/zlib.ts';
-import { zlib } from 'https://deno.land/std@0.162.0/node/internal_binding/constants.ts';
+import { calculatePasses } from "../Interlace.js";
+import normalize from "../Normalize.js";
+import toBitmap from "../Bitmap.js";
+import filter from "../Filter.js";
 
-import { calculatePasses } from '../Interlace.js'
-import normalize from '../Normalize.js'
-import toBitmap from '../Bitmap.js'
-import filter from '../Filter.js'
-
-
-const { max , floor } = Math;
-const { log } = console;
-
+const { max, floor } = Math;
 
 /**
  *  Parses the image data chunk.
  */
- 
-export default async function parse ( data , png ){
 
-    const { width , height , bpp , depth , interlace } = png;
-
+export default async function parse(data, png) {
+    const { width, height, bpp, depth, interlace } = png;
 
     /*
      *  Combine Data Chunks
      */
 
-    data = new Uint8Array(
-        data
-        .map((a) => [...a])
-        .flat()
-    )
-
-    log(data)
-
+    data = new Uint8Array(data.map((a) => [...a]).flat());
 
     /*
      *  Decompress
      */
 
     let options = {};
-    
-    if(!png.interlace){
-        
-        const size = height * (floor((width * bpp * depth + 7) * .125) + 1);
-        
-        options.chunkSize = max(size,zlib.Z_MIN_CHUNK);
+
+    if (!png.interlace) {
+        const size = height * (floor((width * bpp * depth + 7) * 0.125) + 1);
+
+        options.chunkSize = max(size, zlib.Z_MIN_CHUNK);
     }
-    
+
     await new Promise((resolve) => {
-        // interlace 
-        //     ? inflate(data,resolve) 
+        // interlace
+        //     ? inflate(data,resolve)
         //     : inflate(data,options,resolve);
-            
-        inflate(data,options,(error,buffer) => {
+
+        inflate(data, options, (error, buffer) => {
             data = buffer;
             resolve();
         });
-    })
-    
-    log(data);
-    
-    if(!data)
-        throw `Inflate wasn't created`;
+    });
 
-    if(!data.length)
-        throw `Invalid inflate length`;
+    if (!data) throw `Inflate wasn't created`;
 
+    if (!data.length) throw `Invalid inflate length`;
 
     png.data = data;
-    png.passes = interlace 
-        ? calculatePasses(width,height) 
-        : [ png ];
-
+    png.passes = interlace ? calculatePasses(width, height) : [png];
 
     /*
      *  Process
--- Source/png/Parse.js
+++ Source/png/Parse.js
@@ -1,62 +1,47 @@
-
-
-
 // import { inflate } from 'https://deno.land/x/denoflate@1.2.1/mod.ts';
 // import { inflate } from 'https://deno.land/x/zlib.es@v1.0.0/inflate.ts';
 
+import { deepMerge } from "https://deno.land/std@0.162.0/collections/mod.ts";
 
-import { deepMerge } from 'https://deno.land/std@0.162.0/collections/mod.ts';
+import parseTransparency from "./Chunks/Transparency.js";
+import parseImageData from "./Chunks/ImageData.js";
+import parsePalette from "./Chunks/Palette.js";
 
-import parseTransparency from './Chunks/Transparency.js'
-import parseImageData from './Chunks/ImageData.js'
-import parsePalette from './Chunks/Palette.js'
-
-
-const { log } = console;
-
-const
-    signature = [ 0x89 , 0x50 , 0x4e , 0x47 , 0x0d , 0x0a , 0x1a , 0x0a ],
+const signature = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a],
     colorTypeToBPP = {
-        0 : 1 , // Grayscale (1)
-        2 : 3 , // RGB (3)
-        3 : 1 , // Indexed (Palette) (1)
-        4 : 2 , // Grayscale (1) + Alpha (1)
-        6 : 4   // RGB (3) + Alpha (1)
+        0: 1, // Grayscale (1)
+        2: 3, // RGB (3)
+        3: 1, // Indexed (Palette) (1)
+        4: 2, // Grayscale (1) + Alpha (1)
+        6: 4, // RGB (3) + Alpha (1)
     };
 
-
-
-export default async function parse(buffer){
-    
-    function byteAt(buffer,offset){
+export default async function parse(buffer) {
+    function byteAt(buffer, offset) {
         return buffer[offset];
     }
-    
-    function wordAt(buffer,offset){
-        return buffer[offset + 0] << 8 
-             | buffer[offset + 1] << 0 ;
+
+    function wordAt(buffer, offset) {
+        return (buffer[offset + 0] << 8) | (buffer[offset + 1] << 0);
     }
-    
-    function dwordAt(buffer,offset){
-        return wordAt(buffer,offset + 0) << 16
-             | wordAt(buffer,offset + 2) <<  0 ;
+
+    function dwordAt(buffer, offset) {
+        return (
+            (wordAt(buffer, offset + 0) << 16) |
+            (wordAt(buffer, offset + 2) << 0)
+        );
     }
-    
-    
 
     /*
      *  Signature
      */
 
     {
-        
-        for(let i = 0;i < 8;i++)
-            if(signature[i] !== buffer[i])
-                throw `Buffer data has non-png signature: [${ buffer }]`;
-    
+        for (let i = 0; i < 8; i++)
+            if (signature[i] !== buffer[i])
+                throw `Buffer data has non-png signature: [${buffer}]`;
     }
 
-    
     // function v(o){
     //     let val =         buffer[o];
     //     if(val === 152)
@@ -64,7 +49,7 @@ export default async function parse(buffer){
     //     return '\t' + val;
     // }
 
-    // 
+    //
     // for(let b = 0;b < buffer.length;b += 8)
     // log(
     //     v(b + 0) ,
@@ -77,9 +62,8 @@ export default async function parse(buffer){
     //     v(b + 7) ,
     // )
 
-    const chunks = new Map;
-    chunks.set('IDAT',[]);
-
+    const chunks = new Map();
+    chunks.set("IDAT", []);
 
     /*
      *  Read Chunks
@@ -88,130 +72,107 @@ export default async function parse(buffer){
     {
         let offset = 8;
 
-        while(offset < buffer.length){
+        while (offset < buffer.length) {
+            const size = dwordAt(buffer, offset);
 
-            const size = dwordAt(buffer,offset);
-                
-            const type = buffer
-                .subarray(offset += 4,offset += 4);
-                    
-            const data = buffer
-                .subarray(offset,offset += size);
-            
-            const checksum = buffer
-                .subarray(offset,offset += 4);
-                
-                
-            const block = new TextDecoder()
-                .decode(type);
+            const type = buffer.subarray((offset += 4), (offset += 4));
 
+            const data = buffer.subarray(offset, (offset += size));
 
-            log(type,block)
+            const checksum = buffer.subarray(offset, (offset += 4));
 
-            if(block !== 'IDAT'){
-                chunks.set(block,data);
+            const block = new TextDecoder().decode(type);
+
+            if (block !== "IDAT") {
+                chunks.set(block, data);
                 continue;
             }
-                
-            chunks
-            .get('IDAT')
-            .push(data);
+
+            chunks.get("IDAT").push(data);
         }
     }
 
-    
-    log(chunks);
-
-
     let png;
-
 
     /*
      *  Properties
      */
 
     {
-        const
-            data = chunks.get('IHDR'),
+        const data = chunks.get("IHDR"),
             type = data[9];
 
-        const
-            width = dwordAt(data,0) ,
-            height = dwordAt(data,4) ;
+        const width = dwordAt(data, 0),
+            height = dwordAt(data, 4);
 
         png = {
-
             bpp: colorTypeToBPP[type],
 
             size: height * width * 4,
 
-            color:       Boolean(type & 0x2),
+            color: Boolean(type & 0x2),
             transparent: Boolean(type & 0x4),
 
-            depth:       data[8],
-            filter:      data[11],
-            interlace:   data[12],
+            depth: data[8],
+            filter: data[11],
+            interlace: data[12],
             compression: data[10],
 
-            type , width , height
-        }
-        
-        log(png)
+            type,
+            width,
+            height,
+        };
     }
-    
-    
+
     //  Order Matters
-    
+
     const chunkTypes = [
-        [ 'PLTE' , parsePalette ] ,
-        [ 'tRNS' , parseTransparency ] ,
-        [ 'IDAT' , parseImageData ]
-    ]
-    
-    for ( const [ type , parser ] of chunkTypes ){
-        
+        ["PLTE", parsePalette],
+        ["tRNS", parseTransparency],
+        ["IDAT", parseImageData],
+    ];
+
+    for (const [type, parser] of chunkTypes) {
         const data = chunks.get(type);
-        
-        if(data){
-            
-            const parsed = await parser(data,png) ?? {};
-            
-            png = deepMerge(png,parsed);
+
+        if (data) {
+            const parsed = (await parser(data, png)) ?? {};
+
+            png = deepMerge(png, parsed);
         }
     }
-    
 
     // /*
     //  *  Color Palette
     //  */
-    // 
+    //
     // if(chunks.has('PLTE')){
-    // 
+    //
     //     const
     //         palette = [],
     //         data = chunks.get('PLTE');
-    // 
+    //
     //     for(let d = 0,p = 0;d < data.length / d;d += 3,p++)
     //         palette[p] = [ data[d] , data[d + 1] , data[d + 2] , 255 ];
-    // 
+    //
     //     png.palette = palette;
     // }
 
-    // 
+    //
     // /*
     //  *  Transpancy
     //  */
-    // 
+    //
     // if(chunks.has('tRNS')){
-    // 
+    //
     //     png.transparent = true;
-    // 
+    //
     //     const
     //         { type , palette } = png ,
     //         data = chunks.get('tRNS') ;
-    // 
+    //
     //     let offset = 0;
-    // 
+    //
     //     switch(type){
     //     case 0: // Grayscale
     //         png.transparency = [ readByte() ];
@@ -223,7 +184,7 @@ export default async function parse(buffer){
     //         data.forEach((alpha,index) => palette[3] = alpha);
     //         break;
     //     }
-    // 
+    //
     //     function readByte(){
     //         const byte = byteAt(data,offset);
     //         offset++;
@@ -231,73 +192,71 @@ export default async function parse(buffer){
     //     }
     // }
 
-
-
     // /*
     //  *  Image Data
     //  */
-    // 
+    //
     // {
     //     const { width , height , bpp , depth , interlace } = png;
-    // 
+    //
     //     let data = chunks.get('IDAT');
-    // 
-    // 
+    //
+    //
     //     /*
     //      *  Combine Data Chunks
     //      */
-    // 
+    //
     //     data = new Uint8Array(
     //         data
     //         .map((a) => [...a])
     //         .flat()
     //     );
-    // 
+    //
     //     log(data)
-    // 
+    //
     //     /*
     //      *  Decompress
     //      */
-    // 
+    //
     //     let options = {};
-    // 
+    //
     //     if(!png.interlace){
-    // 
+    //
     //         const size = height * (floor((width * bpp * depth + 7) * .125) + 1);
-    // 
+    //
     //         options.chunkSize = max(size,zlib.Z_MIN_CHUNK);
     //     }
-    // 
+    //
     //     await new Promise((resolve) => {
-    //         // interlace 
-    //         //     ? inflate(data,resolve) 
+    //         // interlace
+    //         //     ? inflate(data,resolve)
     //         //     : inflate(data,options,resolve);
-    // 
+    //
     //         inflate(data,options,(error,buffer) => {
     //             data = buffer;
     //             resolve();
     //         });
     //     })
-    // 
+    //
     //     log(data);
-    // 
+    //
     //     if(!data)
     //         throw `Inflate wasn't created`;
-    // 
+    //
     //     if(!data.length)
     //         throw `Invalid inflate length`;
-    // 
-    // 
+    //
+    //
     //     png.data = data;
-    //     png.passes = interlace 
-    //         ? calculatePasses(width,height) 
+    //     png.passes = interlace
+    //         ? calculatePasses(width,height)
     //         : [ png ];
-    // 
-    // 
+    //
+    //
     //     /*
     //      * Process
     //      */
-    // 
+    //
     //     filter(png);
     //     toBitmap(png);
     //     normalize(png);
